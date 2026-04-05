a9dde768fd6c19a1847604f0de91691105a2d41b
Rakib Sarker <mdrakibsarkar1@gmail.com>
2026-04-05 23:46:17 +0600

    add background video in hero section

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import heroImg from "../../public/603848285_2118057782275193_7694275342413896210_n.jpg";
 import Link from "next/link";
 import { useEffect } from "react";
 import { FaEnvelope, FaLinkedin, FaGithub, FaWhatsapp } from "react-icons/fa";
@@ -39,91 +38,101 @@ export default function Hero({
   };
 
   return (
-    <section className="flex items-center text-white py-40">
-      <title>RAKIB SARKER</title>
-      <div className="max-w-6xl mx-auto px-6 w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
-          {/* 🔥 Left Content */}
-          <motion.div variants={container} initial="hidden" animate="show">
-            <motion.p
-              className="text-sm font-medium text-green-300 mb-3"
-              variants={fadeUp}
-            >
-              🚀 Frontend developer
-            </motion.p>
-
-            <motion.h1
-              className="text-4xl md:text-5xl font-extrabold leading-tight"
-              variants={fadeUp}
-            >
-              {title}
-            </motion.h1>
-
-            <motion.p className="mt-4 text-lg max-w-lg" variants={fadeUp}>
-              {subtitle}
-            </motion.p>
-
-            <motion.div className="mt-6 flex gap-4" variants={fadeUp}>
-              <motion.div
-                whileHover={{ scale: 1.08 }}
-                whileTap={{ scale: 0.95 }}
-                className="bg-white text-gray-900 px-5 py-2 rounded-md font-medium"
+    <>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="fixed top-0 left-0 w-full h-full object-cover z-10"
+      >
+        <source src="/13522169-uhd_3840_2160_25fps.mp4" type="video/mp4" />
+      </video>
+      <section className="flex items-center text-white py-40 z-12 relative -mt-13 sm:mt-2 md:mt-0">
+        <title>RAKIB SARKER</title>
+        <div className="max-w-6xl mx-auto px-6 w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+            {/* 🔥 Left Content */}
+            <motion.div variants={container} initial="hidden" animate="show">
+              <motion.p
+                className="text-sm font-medium text-green-300 mb-3"
+                variants={fadeUp}
               >
-                <Link href="/projects">Projects</Link>
+                🚀 Frontend developer
+              </motion.p>
+
+              <motion.h1
+                className="text-4xl md:text-5xl font-extrabold leading-tight"
+                variants={fadeUp}
+              >
+                {title}
+              </motion.h1>
+
+              <motion.p className="mt-4 text-lg max-w-lg" variants={fadeUp}>
+                {subtitle}
+              </motion.p>
+
+              <motion.div className="mt-6 flex gap-4" variants={fadeUp}>
+                <motion.div
+                  whileHover={{ scale: 1.08 }}
+                  whileTap={{ scale: 0.95 }}
+                  className="bg-white text-gray-900 px-5 py-2 rounded-md font-medium"
+                >
+                  <Link href="/projects">Projects</Link>
+                </motion.div>
+
+                <motion.div
+                  whileHover={{ scale: 1.08 }}
+                  whileTap={{ scale: 0.95 }}
+                  className="border border-white px-5 py-2 rounded-md font-medium hover:bg-white hover:text-gray-900 transition"
+                >
+                  <Link href="/about/contact">Contact</Link>
+                </motion.div>
               </motion.div>
-
               <motion.div
-                whileHover={{ scale: 1.08 }}
-                whileTap={{ scale: 0.95 }}
-                className="border border-white px-5 py-2 rounded-md font-medium hover:bg-white hover:text-gray-900 transition"
+                className="flex items-center gap-4 text-gray-300 text-xl mt-5"
+                variants={fadeUp}
               >
-                <Link href="/about/contact">Contact</Link>
+                {/* Email */}
+                <a
+                  href="mailto:mdrakibsarkar1@gmail.com"
+                  target="_blank"
+                  className="hover:text-white transition"
+                >
+                  <FaEnvelope />
+                </a>
+
+                {/* LinkedIn */}
+                <a
+                  href="https://www.linkedin.com/in/rakib-sarker-"
+                  target="_blank"
+                  className="hover:text-white transition"
+                >
+                  <FaLinkedin />
+                </a>
+
+                {/* GitHub (X er jaygay usually GitHub use kore dev ra 😏) */}
+                <a
+                  href="https://github.com/Sark-Rakib"
+                  target="_blank"
+                  className="hover:text-white transition"
+                >
+                  <FaGithub />
+                </a>
+
+                {/* WhatsApp */}
+                <a
+                  href="https://wa.me/8801745762857"
+                  target="_blank"
+                  className="hover:text-white transition"
+                >
+                  <FaWhatsapp />
+                </a>
               </motion.div>
             </motion.div>
-            <motion.div
-              className="flex items-center gap-4 text-gray-300 text-xl mt-5"
-              variants={fadeUp}
-            >
-              {/* Email */}
-              <a
-                href="mailto:mdrakibsarkar1@gmail.com"
-                target="_blank"
-                className="hover:text-white transition"
-              >
-                <FaEnvelope />
-              </a>
 
-              {/* LinkedIn */}
-              <a
-                href="https://www.linkedin.com/in/rakib-sarker-"
-                target="_blank"
-                className="hover:text-white transition"
-              >
-                <FaLinkedin />
-              </a>
-
-              {/* GitHub (X er jaygay usually GitHub use kore dev ra 😏) */}
-              <a
-                href="https://github.com/Sark-Rakib"
-                target="_blank"
-                className="hover:text-white transition"
-              >
-                <FaGithub />
-              </a>
-
-              {/* WhatsApp */}
-              <a
-                href="https://wa.me/8801745762857"
-                target="_blank"
-                className="hover:text-white transition"
-              >
-                <FaWhatsapp />
-              </a>
-            </motion.div>
-          </motion.div>
-
-          {/* 🔥 Right Image */}
-          {/* <motion.div
+            {/* 🔥 Right Image */}
+            {/* <motion.div
             initial={{ opacity: 0, scale: 0.8, rotate: -10 }}
             animate={{ opacity: 1, scale: 1, rotate: 0 }}
             transition={{ duration: 1, ease: "easeOut" }}
@@ -146,8 +155,9 @@ export default function Hero({
                 />
             </motion.div>
           </motion.div> */}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
